d87f549ad05ab933fb149c9c5bfa283615160188
David Keijser <keijser@gmail.com>
2015-04-14 10:45:13 +0200

    update zsh completion script
    Rewrite to use the `_arguments` helper and a function providing the
    fixup bases which also was improved to not call git-fixup twice.
    
    If git-fixup does not yield any commits to use the output to stderr will
    be displayed using `_message`
    
    Fixes #19

--- completion.zsh
+++ completion.zsh
@@ -1,7 +1,15 @@
 #compdef git-fixup
-local lines words
 
-lines=(${(f)"$(git fixup)"})
-words=(${(f)"$(git fixup | cut -d' ' -f 1)"})
+function _fixup_target {
+    local -a lines commits
 
-compadd -l -d lines -a -- words
+    lines=(${(f)"$(git fixup)"}) 2>&1 | read err
+    if [ $#lines -eq 0 ]; then
+        _message $err
+        return 1
+    fi
+    commits=(${lines[@]%% *})
+    compadd -l -d lines -a -- commits
+}
+
+_arguments ':commit:_fixup_target'
