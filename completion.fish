537ef9aa789b08b832cca1e09ca547f3bc43677f
Philippe Eberli <p.eberli@gmx.ch>
2021-08-22 21:32:30 +0200

    Add tab completion for the fish shell

---
+++ completion.fish
@@ -0,0 +1,10 @@
+# fish completion for git fixup
+
+function __fish_git_fixup_target
+    git fixup --no-commit 2>/dev/null | string replace -r '^([0-9a-f]{10})[0-9a-f]* (.*)' '$1\t$2'
+end
+
+complete -c git -n '__fish_git_using_command fixup' -s s -l squash -f -d 'create a squash commit rather than a fixup'
+complete -c git -n '__fish_git_using_command fixup' -s c -l commit -f -d 'show a menu to pick a commit'
+complete -c git -n '__fish_git_using_command fixup' -l no-commit -f -d 'don\'t show a menu to pick a commit'
+complete -c git -n '__fish_git_using_command fixup' -f -k -a '(__fish_git_fixup_target)'
